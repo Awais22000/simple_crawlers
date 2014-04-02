362d969118631e75615d7b8b17a93cf0d53f5a73
Jovian Lin <jovian.lin@gmail.com>
2014-04-02 15:25:37 +0800

    Create simple_googleplus_crawler.py

---
+++ simple_googleplus_crawler.py
@@ -0,0 +1,20 @@
+import requests
+
+# These are the accounts/handles for which you will fetch data from.
+list_of_handles = [
+    'JovianLin',
+    'BarackObama',
+]
+
+# Google Plus API base URL.
+base_url = 'https://www.googleapis.com/plus/v1/people/'
+api_key  = 'SOME_KEY'
+
+# Iterate over the handles and trigger the API with each.
+for handle in list_of_handles:
+    url = base_url + '+' + handle + '?key=' + api_key
+    response = requests.get(url)
+    profile = response.json()
+    print profile
+    print '\n'
+# End of for loop.
