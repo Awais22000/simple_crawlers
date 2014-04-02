dda8e060f4272894ba04f3c1928b21a535b687ad
Jovian Lin <jovian.lin@gmail.com>
2014-04-02 15:30:58 +0800

    Update simple_googleplus_crawler.py

--- simple_googleplus_crawler.py
+++ simple_googleplus_crawler.py
@@ -8,7 +8,7 @@ list_of_handles = [
 
 # Google Plus API base URL.
 base_url = 'https://www.googleapis.com/plus/v1/people/'
-api_key  = 'SOME_KEY'
+api_key  = 'SOME_KEY'  # Create this key via "Public API access" in https://console.developers.google.com
 
 # Iterate over the handles and trigger the API with each.
 for handle in list_of_handles:
